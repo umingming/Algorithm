d53aacfbfa9006677c7cf0bac3c12dde4a8eefb0
umingming <u_0112@naver.com>
2021-08-11 23:02:40 +0900

    윤년

---
+++ STEP01/No2753.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class No2753 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		
+		if(A%4 == 0 && A%400 == 0)
+			System.out.println("1");
+		else if(A%4 == 0 && A%100 != 0)
+			System.out.println("1");
+		else System.out.println("0");
+	}
+}
