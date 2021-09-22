f627ced8a516a3d9fff33a267a6831976968b59c
umingming <u_0112@naver.com>
2021-09-22 15:02:47 +0900

    scanner로 입력 받음

--- BAEKJOON/src/step01/No1000.java
+++ BAEKJOON/src/step01/No1000.java
@@ -1,14 +1,14 @@
 package step01;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class No1000 {
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(reader.readLine());
-		int b = Integer.parseInt(reader.readLine());
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
 		
 		System.out.println(a + b);
 
