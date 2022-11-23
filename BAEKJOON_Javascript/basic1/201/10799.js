3ac240fbbfc3949b5a353bd2d4199916092f3901
umingming <u_0112@naver.com>
2022-11-23 15:27:00 +0900

    이상하게 품

---
+++ BAEKJOON_Javascript/basic1/201/10799.js
@@ -0,0 +1,43 @@
+/*
+    쇠 막대기
+    1. 입력 모듈 선언
+    2. solution 호출
+        > sticks, cnt 변수 선언
+        > line 길이만큼 반복
+            > if (인지?
+                > sticks에 push
+                > 아니면, pop하고 cnt에 sticks 길이 추가
+        > cnt 출력
+*/
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    solution(line);
+    rl.close();
+});
+
+rl.on("close", () => {
+    process.exit();
+})
+
+function solution(line) {
+    let result = 0;
+    let sticks = [];
+    let brackets = line.split('');
+
+    for (let i in brackets) {
+        if (brackets[i] === '(') {
+            sticks.push('-');
+        } else {
+            sticks.pop();
+            result += sticks.length;
+        }
+    }
+
+    console.log(result);
+}
