18525af0d78b6206e988c234c95527876f0b7690
umingming <u_0112@naver.com>
2022-12-29 10:15:17 +0900

    시간초과

---
+++ BAEKJOON_Javascript/basic1/300/1676.js
@@ -0,0 +1,33 @@
+/*
+    팩토리얼 구하기
+    1. result 변수 후 입력 값 할당
+    2. while문으로 숫자가 0이 아니면 반복
+        > result에 num 곱하기
+    3. 0이면 1 출력
+*/
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    solution(+line);
+});
+
+function solution(num) {
+    let result = 1;
+    let cnt = 0;
+
+    while (num) {
+        result *= num--;
+    }
+
+    while (!(result % 10)) {
+        result /= 10;
+        cnt++;
+    }
+    
+    console.log(cnt);
+}
