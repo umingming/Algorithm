ece5e93fdba4e706df1e8c30ab13c8aeb8fc811e
umingming <u_0112@naver.com>
2022-12-29 09:51:24 +0900

    get factorial

---
+++ BAEKJOON_Javascript/basic1/300/10872.js
@@ -0,0 +1,27 @@
+/*
+    팩토리얼 구하기
+    1. result 변수 후 입력 값 할당
+    2. while문으로 숫자가 0이 아니면 반복
+        > result에 num 곱하기
+    3. 0이면 1 출력
+*/
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    solution(+line);
+});
+
+function solution(num) {
+    let result = num;
+
+    while (num > 1) {
+        result *= --num;
+    }
+
+    console.log(result || 1);
+}
