fd2d723ac72c90895e5d91e37fb00158f9d6879e
umingming <u_0112@naver.com>
2022-12-30 11:52:58 +0900

    test

--- BAEKJOON_Javascript/basic1/301/1373.js
+++ BAEKJOON_Javascript/basic1/301/1373.js
@@ -6,15 +6,15 @@
         > 입력을 쪼개 배열에 할당  
         > result 변수 선언
         > while문 배열의 길이
-            > pop이 0이 아니면 1; input.pop() && 1
-            > input.pop() && 2
-            > input.pop() && 4
+            > pop이 0이 아니면 1; binary.pop() && 1
+            > binary.pop() && 2
+            > binary.pop() && 4
 */
 
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
+    binary: process.stdin,
     output: process.stdout,
 });
 
@@ -24,28 +24,15 @@ rl.on("line", (line) => {
 });
 
 function solution(line) {
-    let binary = line.split("");
-    let result = "";
-    let octal = 0;
-    let index = 1;
+    let binary = line.split("").map(i => +i);
+    let octal = "";
 
     while (binary.length) {
-        octal += +binary.pop() && index;
-        index *= 2;
-
-        if (index > 4) {
-            result = octal + result;
-            octal = 0;
-            index = 1;
-        }
-
-
-        
-        // let result = 0;
-        // result += (+binary.pop() && 1) || 0;
-        // result += (+input.pop() && 2) || 0;
-        // result += (+input.pop() && 4) || 0;
-        // results = result + results;
+        octal = toOctal(binary.pop(), binary.pop(), binary.pop()) + octal;
     }
-    console.log(result);
+    console.log(octal);
+}
+
+function toOctal(x = 0, y = 0, z = 0) {
+    return x && 1 + y && 2 + z && 4;
 }
