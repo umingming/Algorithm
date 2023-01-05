d5d69e7a4816c9f57ab53f4fbc3c5e476f710599
umingming <u_0112@naver.com>
2023-01-05 15:20:09 +0900

    Solve 10 to n

---
+++ BAEKJOON_Javascript/basic1/302/11005.js
@@ -0,0 +1,25 @@
+/*
+    골드바흐 파티션
+
+    1. 입력값을 쪼개 n, b에 할당
+    2. toString 사용해 진법 변환 후 대문자 출력
+*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    solution(line);
+});
+
+function solution(line) {
+    const [n, b] = line.split(" ").map(i => +i);
+
+    const result = n.toString(b).toUpperCase();
+
+    console.log(result);
+}
