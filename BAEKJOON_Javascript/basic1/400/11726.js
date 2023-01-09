7dca180dd10e89b330de3a56f0835105aec04d01
umingming <u_0112@naver.com>
2023-01-09 09:16:13 +0900

    1이면 어카지

---
+++ BAEKJOON_Javascript/basic1/400/11726.js
@@ -0,0 +1,35 @@
+/*
+    2 x n 타일링
+    - 10007로 나눈 나머지
+
+    1. 1, 2를 요소로 하는 배열 선언
+    2. length가 num 보다 작을 때까지 반복
+        > x, y에 pop한 값을 할당
+        > 배열에 x, x+y 추가
+    3. pop한 값을 10007로 나눈 나머지 출력
+*/
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on("line", (line) => {
+    solution(+line);
+});
+
+function solution(num) {
+    let result = [1, 2];
+    
+    while (num-- > result.length) {
+        let x = result.pop();
+        let y = result.pop();
+        
+        result.push(x);
+        result.push(x + y)
+    }
+
+    console.log(result.pop() % 10007);
+}
