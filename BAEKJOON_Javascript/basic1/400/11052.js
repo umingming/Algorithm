60354c819972fd5b6c01c612b0758e352abc80a6
umingming <u_0112@naver.com>
2023-01-10 16:17:03 +0900

    에고

--- BAEKJOON_Javascript/basic1/400/11052.js
+++ BAEKJOON_Javascript/basic1/400/11052.js
@@ -4,7 +4,10 @@
     1. N과 배얼 P 선언
     2. 입력을 쪼개 P에 할당
     3. 배열 반복
-        > 해당 요소랑 이전 요소 + 첫 번째 요소랑 더한 값 중 최대값 재할당
+        > 현재 요소의 값이 이전 값 + 첫 요소보다 큰지?
+            > standard에 i 할당
+        > i + 1이 standard로 나누어 떨어지는지
+            > i + 1 / standard * P 저장
     4. 마지막 값 꺼내기
 */
 
@@ -27,13 +30,18 @@ rl.on("line", (line) => {
 
 function solution(line) {
     let P = line.split(" ").map(i => +i);
+    let j = N + 1;
     for (let i = 1; i < N; i++) {
-        P[i] = Math.max(P[i-1] + P[0], P[i]);
-
-        if (i > 1 && i % 2) {
-            P[i] = Math.max(P[i-2] + P[1], P[i]);
-        } 
-        console.log(P);
+        
+        if (P[i] > P[i-1] + P[0]) {
+            j = P[j-1] / j > P[i] / (i + 1) ? j : i + 1;
+        } else {
+            P[i] = P[i-1] + P[0];
+        }
+        
+        if ((i + 1) % j === 0) {
+            P[i] = (i + 1) / j * P[j-1];
+        }
     }
     console.log(P.pop());
 }
