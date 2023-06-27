6e2368c6d083c2cef8e62c9b40f849e3837c1f5e
umingming <u_0112@naver.com>
2023-06-27 20:00:38 +0900

    feat: Update while loop condition

--- Javascript/Programmers/level1/17.js
+++ Javascript/Programmers/level1/17.js
@@ -2,23 +2,18 @@
     햄버거 갯수 구하기
     - 1, 2, 3이 한 세트
     
-    1. pattenr 선언 /(123)/
+    1. pattenr 선언 /(1231)/
     2. ingredient를 join해서 문자열로 만들고 pattern을 match해서 index값 찾기
     3. ingredient를 splice로 자르고, 계속 반복
+    
 */
 function solution(ingredient) {
-    const pattern = /(123)/;
-    let flag = true;
+    ingredient = ingredient.join("");
     let count = 0;
     
-    while (flag) {
-        const index = ingredient.join("").match(pattern)?.index ?? -1;
-        if (index > -1) {
-            ingredient.splice(index, 3);
-            count++;
-        } else {
-            flag = false;
-        }
+    while (ingredient.includes("1231")) {
+        ingredient = ingredient.replace("1231", "");
+        count++;
     }
     return count;
 }
