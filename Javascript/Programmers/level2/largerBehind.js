04e2fca14022851acac76d1b6f2b551cd71e32bb
umingming <u_0112@naver.com>
2023-07-29 15:26:52 +0900

    timeOut

--- Javascript/Programmers/level2/largerBehind.js
+++ Javascript/Programmers/level2/largerBehind.js
@@ -1,18 +1,19 @@
 /*
-    뒤에 있는 큰 수 찾기
-    - 없을 경우 -1
+    뒷 큰 수 찾기
+    - 이전 요소가 나보다 작은데 larger이 나보다 클 경우 그 값 추가
     
-    1. for문 numbers 크기 반복
-        1) i보다 index가 크며, 값이 큰 요소 할당
-        2) 없을 경우 -1
-    2. 해당 answer 반환
-        
+    1. numbers reduce하여 largers 반환
 */
 function solution(numbers) {
-    const answer = [];
-    const largers = numbers.map((num, index) => {
-        const larger = numbers.slice(index + 1).find(i => i > num) ?? -1;
-        return larger;
-    })
+    const largers = numbers.reduce((acc, cur, index) => {
+        let larger = -1;
+        if (numbers[index - 1] < cur && acc.at(-1) > cur) {
+            larger = acc.at(-1);
+        } else {
+            const larger = numbers.find((num, i) => i > index && num > cur) ?? -1
+            acc.push(larger);
+        }
+        return acc;
+    }, []);
     return largers;
 }
