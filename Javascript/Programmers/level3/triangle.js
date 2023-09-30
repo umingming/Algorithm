ede974d4244b1d754a9209d70ff2c6df93779fae
umingming <u_0112@naver.com>
2023-09-30 15:08:59 +0900

    feat: Loop triange

--- Javascript/Programmers/level3/triangle.js
+++ Javascript/Programmers/level3/triangle.js
@@ -6,11 +6,13 @@
         1) acc자체를 cur map으로 반환한 것의 concat으로 map하기
 */
 function solution(triangle) {
-    const answer = triangle.reduce((acc, cur) => {
-        return acc.map(([index, sum]) => 
-                ([[ index, cur[index] + sum],
-                [ index + 1, cur[index + 1] + sum]])
-        ).flat();
-    }, [[0, 0]])
-    return Math.max(...answer.map(([index, sum]) => sum || 0));
+    const sums = triangle.reduce((acc, cur) => {
+        const answer = [];
+        acc.forEach(([index, sum]) => {
+           answer.push([index, cur[index] + sum]);
+           answer.push([index + 1, cur[index + 1] + sum]);
+        });
+        return answer;
+    }, [[0, 0]]).map(([index, sum]) => sum || 0);
+    return Math.max(...sums);
 }
