a5baa8a688fb1596f831a8587948b890e939bd44
umingming <u_0112@naver.com>
2023-09-30 15:23:43 +0900

    feat: transfrom Object map

--- Javascript/Programmers/level3/triangle.js
+++ Javascript/Programmers/level3/triangle.js
@@ -6,13 +6,15 @@
         1) acc자체를 cur map으로 반환한 것의 concat으로 map하기
 */
 function solution(triangle) {
-    const sums = triangle.reduce((acc, cur) => {
-        const answer = [];
+    const sums = triangle.slice(1).reduce((acc, cur) => {
+        const indexMap = {};
         acc.forEach(([index, sum]) => {
-           answer.push([index, cur[index] + sum]);
-           answer.push([index + 1, cur[index + 1] + sum]);
+            const left = indexMap[index] ?? 0;
+            const right = indexMap[+index + 1] ?? 0;
+            indexMap[index] = left >= +cur[index] + sum ? left : +cur[index] + sum;
+            indexMap[+index + 1] = right >= +cur[+index + 1] + sum ? right : +cur[+index + 1] + sum;       
         });
-        return answer;
-    }, [[0, 0]]).map(([index, sum]) => sum || 0);
+        return Object.entries(indexMap);
+    }, [[0, +triangle[0]]]).map(([index, sum]) => sum || 0);
     return Math.max(...sums);
 }
