e1a836d54dd2bc0689a4148be74a3456f427c745
umingming <u_0112@naver.com>
2023-12-10 17:48:05 +0900

    concatenate Elements

--- Javascript/Programmers/level2/partSum.js
+++ Javascript/Programmers/level2/partSum.js
@@ -1,42 +1,20 @@
 /*
-    연속 부분 수열 합의 갯수 구하기
-    - Set 사용
-    
-    1. 1부터 elements 크기만큼 반복
-    2. i를 길이로 하는 부분 수열의 합을 set에 추가
-    3. set의 갯수 반환
+    발상의 전환
+    count는 배열의 크기보다 커질 수 없으므로 배열을 하나 더 이어붙이면 복잡하게 생각할 필요가 없어짐.
 */
 function solution(elements) {
     const sumSet = new Set();
+    const {length} = elements; 
     
-    /*
-        길이를 인자로 부분 수열의 합들 구하기
-        
-        1. 요소 기본 for문으로 반복
-        2. if 길이 - i > 인자 - 1인지?
-            > 요소를 인덱스부터 인자까지 slice한 배열의 합 결과에 추가
-        3. 요소를 인덱스부터 끝까지 slice한 배열과 0부터 i - 인자 slice한 배열의 합 결과에 추가
-    */
-    function validateSums(length) {
-        for (let i = 0; i < elements.length; i++) {
-            let part = [];
-            const endIndex = i + length;
-            
-            if (elements.length - i > length - 1) {
-                part = elements.slice(i, endIndex);
-            } else {
-                part = [...elements.slice(i), ...elements.slice(0, i - endIndex)]
-            }
-            
-            sumSet.add(part.reduce((acc, cur) => acc + cur, 0));
+    elements = [...elements, ...elements];
+    
+    for (let count = 1; count <= length; count++) {
+        for (let index = 0; index < length; index++) {
+            const sum = elements.slice(index, index + count)
+                                .reduce((acc, cur) => acc + cur, 0);
+            sumSet.add(sum);
         }
     }
     
-    for (let i = 1; i <= elements.length; i++) {
-        for 
-        validateSums(i);
-    }
-    
-    const {size} = sumSet;
-    return size;
+    return sumSet.size;
 }
