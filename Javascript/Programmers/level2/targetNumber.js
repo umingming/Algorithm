dbcd9948e8ed6e49bdd4086724bf58d97b070a69
umingming <u_0112@naver.com>
2024-02-04 21:51:21 +0900

    dfs/bfs

---
+++ Javascript/Programmers/level2/targetNumber.js
@@ -0,0 +1,19 @@
+/*
+    1. 총합 구하는 함수 정의
+    2. 합의 갯수 중 target인 것 반환
+*/
+function solution(numbers, target) {
+    
+    function sum(index, parent) {
+        if (index === numbers.length) return parent;
+        
+        const num = numbers[index];
+        const totals = [...parent.map(total => total + num), ...parent.map(total => total - num)]
+        return sum(++index, totals);
+    }
+    
+    const result = sum(1, [+numbers[0], -numbers[0]])
+    const {length} = result.filter(value => value === target);
+    
+    return length;
+}
