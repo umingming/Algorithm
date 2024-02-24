393354f0236c04d15830a06be1f3fad0792b8c02
umingming <u_0112@naver.com>
2024-02-25 00:29:47 +0900

    update partSum

--- Javascript/Programmers/level2/partSequence.js
+++ Javascript/Programmers/level2/partSequence.js
@@ -1,48 +1,42 @@
 /*
     부분 수열 찾기
     - 길이가 짧고, 인덱스가 작은 수열이 우선
+    - 부분 수열의 요소들이 다 같을 경우만 예외
     
-    1. sequence를 k의 부분 수열 정의함.
-        1) 이전 요소의 합이 k인 경우: 다음 요소에 cur push
-        2) 이전 요소들과 cur의 합이 k인 경우: 요소에 push
-        3) 이전 요소들의 합과 cur의 합이 k를 초과하는 경우
-            3-1) 처음 index를 제외하고 k가 되는 경우: 0번째 요소 제외하고 push
-            3-2) 안 되면 요소 pop()
-        4) 현재 요소가 k인 경우
-            4-1) 이전 요소의 합이 k인 경우: 새로운 요소에 push
-            4-2) 아닌 경우, pop 후 push
-    2. 마지막 요소의 길이와 같은 부분 수열 요소가 있는지 확인
-    3. 2의 결과로 findIndex 해서 결과 반환
-    
+    1. part 배열 정의
+    2. while sequence 반복
+        1) sequnece.pop 값을 part 배열에 할당
+        2) part의 합이 k보다 커지면?
+            2-1) 0이 아닌 요소의 인덱스를 구해서 0으로 만들기
+        3) part의 합이 k면 중단하고 result 넣기
+    3. if part의 첫 번째와 두번째 합이 같은지?
+        1) part 0 번째 요소 index 찾고 결과 리턴
 */
 function solution(sequence, k) {
-    let partStr = "";
+    const copy = [...sequence];
+    let part = [];
     let partSum = 0;
-    const result = [];
+    let size = 0;
     
-    while (sequence.length && partSum !== k) {
-        const num = sequence.pop();
-        partStr += `${num},`;
+    while (partSum !== k) {
+        const num = copy.pop();
+        part.push(num);
         partSum += num;
+        size++;
         
         if (partSum > k) {
-            const index = partStr.indexOf(",");
-            partSum -= +partStr.substring(0, index);
-            partStr = partStr.substring(index + 1);
+            partSum -= part[part.length - size];
+            size--;
         }
     }
     
-    const part = partStr.split(",");
-    part.pop();
-    result[0] = sequence.length;
-    result[1] = result[0] + part.length - 1;
+    part = part.slice(part.length - size);
+    const { length: start } = copy;
     
-    if (part[0] === part.at(-1)) {
-        const index = sequence.findIndex(i => i == part[0]);
-        if (index > -1) {
-            result[0] = index;
-            result[1] = result[0] + part.length - 1;
-        }
+    if (part[0] === part[size - 1]) {
+        const index = sequence.findIndex(num => num === part[0]);
+        return [index, index + size - 1];
     }
-    return result;
+    
+    return [start, start + size - 1];
 }
