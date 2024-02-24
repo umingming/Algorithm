0918c18288292cb917bbb86a3b67714dde00df37
umingming <u_0112@naver.com>
2024-02-25 00:05:18 +0900

    validate map point

--- Javascript/Programmers/level2/gameMap.js
+++ Javascript/Programmers/level2/gameMap.js
@@ -7,9 +7,21 @@
         > 상, 하, 좌, 우로 움직이면서 맵의 숫자를 재정의한다.
 */
 function solution(maps) {
+    const n = maps.length - 1;
+    const m = maps[0].length - 1;
+    let min = Infinity;
+    
+    
     function move(row, col, step) {
-        const value = maps?.[row]?.[col];
+        if (step + (n - row) + (m - col) > min || row > n || row < 0 || col > m || col < 0) {
+            return;
+        }
+        
+        const value = maps[row][col];
         if (value === 1 || (value && value > step)) {
+            if (row === n && col === m) {
+                min = Math.min(min, step);
+            }
             maps[row][col] = step++;
 
             move(row, col + 1, step);
@@ -21,6 +33,6 @@ function solution(maps) {
     
     move(0, 0, 1);
     
-    const result = maps[maps.length - 1][maps[0].length - 1];
-    return result !== 1 ? result : -1;
+    const result = maps[n][m];
+    return maps[n][m] !== 1 ? result : -1;
 }
